ebcad5260636d466ea0c5b1b6fd32dbf99021943
Heramb Mishra <61630627+DarkShadow1060@users.noreply.github.com>
2022-08-14 18:28:23 +0530

    Update database.js

--- DataBase/database.js
+++ DataBase/database.js
@@ -3,7 +3,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('studentmarksheetmanagementdb','heramb','Abcd@1234#',{
-    host:"studentmarksheetmanagementdb.mysql.database.azure.com",
+    host:"studentmarksheetmanagementdb.cluster-ct0tqivkeuwn.ap-south-1.rds.amazonaws.com",
     dialect:"mysql"
 });
 
